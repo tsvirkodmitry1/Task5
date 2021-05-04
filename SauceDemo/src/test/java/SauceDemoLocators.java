ee28ef872814243e841e4e95cd9946fff4b25956
tsvirkodmitry1 <tsvirkodmitry1@gmail.com>
2021-05-04 23:49:58 +0300

    "repair"

--- SauceDemo/src/test/java/SauceDemoLocators.java
+++ SauceDemo/src/test/java/SauceDemoLocators.java
@@ -7,18 +7,42 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.awt.*;
-import java.util.ArrayList;
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class SauceDemoLocators {
     WebDriver driver;
 
-    public static void main(String[] args) {
-        Product bag = new Product();
-        Product.addPrice(29.99);
-        ProductName name = new ProductName();
-        name.setName("Sauce Labs Backpack");
-        System.out.println(name.name + Product.productsPrice);
+
+    private String userNameField;
+    private String passwordField;
+    private String loginButton;
+    private String loginStandard;
+    private String passwordKey;
+    private String immageProduct;
+    private String productName;
+    private String productDetails;
+    private String productPrice;
+    private String buttonAddToCart;
+
+    public void listXpaths() throws IOException {
+        File file = new File("SauceDemoLocators.txt");
+        Scanner reader = new Scanner(file);
+        String line = reader.nextLine();
+        String[] xpathName = line.split(" ");
+        reader.close();
+        System.out.println(Arrays.toString(xpathName));
+        userNameField = xpathName[0];
+        passwordField = xpathName[1];
+        loginButton = xpathName[2];
+        loginStandard = xpathName[3];
+        passwordKey = xpathName[4];
+        immageProduct = xpathName[5];
+        productName = xpathName[6];
+        productDetails = xpathName[7];
+        productPrice = xpathName[8];
+        buttonAddToCart = xpathName[9];
     }
 
     @BeforeTest
@@ -27,54 +51,56 @@ public class SauceDemoLocators {
         System.setProperty("webdriver.chrome.driver", "src\\chromedriver.exe.exe");
         driver = new ChromeDriver();
         driver.get("https://www.saucedemo.com/");
+
     }
 
     @Test
-    public void sauceDemoLocators() {
+    public void sauceDemoLocators() throws IOException {
         //check items and login
-        WebElement username = driver.findElement(By.xpath("//*[@id=\"user-name\"]"));
-        WebElement password = driver.findElement(By.xpath("//*[@id=\"password\"]"));
-        WebElement login = driver.findElement(By.xpath("//*[@id=\"login-button\"]"));
-        Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"user-name\"]")).isEnabled());
-        Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"password\"]")).isEnabled());
-        Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"login-button\"]")).isEnabled());
-        //login
-        username.sendKeys("standard_user");
-        password.sendKeys("secret_sauce");
+        listXpaths();
+        WebElement userName = driver.findElement(By.xpath(loginStandard));
+        String usersName = userName.getAttribute("innerText");
+        String[] firstUserName = usersName.split("\n");
+        System.out.println(Arrays.toString(firstUserName));
+        WebElement password = driver.findElement(By.xpath(passwordKey));
+        String passwordForLogin = password.getAttribute("innerText");
+        String[] onePassword = passwordForLogin.split("\n");
+        System.out.println(Arrays.toString(onePassword));
+
+        WebElement userFieldName = driver.findElement(By.xpath(userNameField));
+        userFieldName.click();
+        userFieldName.sendKeys(firstUserName[1]);
+        WebElement fieldPassword = driver.findElement(By.xpath(passwordField));
+        fieldPassword.click();
+        fieldPassword.sendKeys(onePassword[1]);
+        Assert.assertTrue(driver.findElement(By.xpath(userNameField)).isEnabled());
+        Assert.assertTrue(driver.findElement(By.xpath(passwordField)).isEnabled());
+        WebElement login = driver.findElement(By.xpath(loginButton));
         login.click();
         Assert.assertTrue(driver.getCurrentUrl().contains("https://www.saucedemo.com/inventory.html"));
     }
 
     @Test
     public void searchElem() {
-        driver.findElement(By.xpath("//*[@id=\"item_4_img_link\"]/img"));
-        driver.findElement(By.xpath("//*[@id=\"item_4_title_link\"]/div"));
-        driver.findElement(By.xpath("//*[@id=\"inventory_container\"]/div/div[1]/div[2]/div[1]/div"));
-        driver.findElement(By.xpath("//*[@id=\"inventory_container\"]/div/div[1]/div[2]/div[2]/div"));
-        driver.findElement(By.xpath("//*[@id=\"add-to-cart-sauce-labs-backpack\"]"));
-        Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"item_4_img_link\"]/img")).isDisplayed());
-        Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"item_4_title_link\"]/div")).isDisplayed());
-        Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"inventory_container\"]/div/div[1]/div[2]/div[1]/div")).isDisplayed());
-        Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"inventory_container\"]/div/div[1]/div[2]/div[2]/div")).isDisplayed());
-        Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"add-to-cart-sauce-labs-backpack\"]")).isDisplayed());
+        driver.findElement(By.xpath(immageProduct));
+        WebElement nameProduct = driver.findElement(By.xpath(productName));
+        driver.findElement(By.xpath(productDetails));
+        WebElement price = driver.findElement(By.xpath(productPrice));
+        driver.findElement(By.xpath(buttonAddToCart));
+        Assert.assertTrue(driver.findElement(By.xpath(immageProduct)).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.xpath(productName)).getText().equalsIgnoreCase("Sauce Labs Backpack"));
+        Assert.assertTrue(driver.findElement(By.xpath(productDetails)).getText().equalsIgnoreCase("carry.allTheThings() with the sleek, " +
+                "streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection."));
+        Assert.assertTrue(driver.findElement(By.xpath(productPrice)).getText().equalsIgnoreCase("$29.99"));
+        Assert.assertTrue(driver.findElement(By.xpath(buttonAddToCart)).isEnabled());
+        System.out.println(nameProduct.getText());
+        System.out.println(price.getText());
+
     }
 
     @AfterTest
     public void postTest() {
-        //driver.quit();
-    }
-
-    public static class Product {
-        public static double productsPrice = 0;
-
-        public static void addPrice(double productsPrice) {
-            Product.productsPrice = Product.productsPrice + productsPrice;
-        }
-    }
-    public static class ProductName{
-        private String name = "Sauce Labs Backpack";
-        public void setName(String name){
-            this.name = name;
-        }
+        driver.quit();
     }
 }
+
